50ce2945deaa5705dd0007666878185f9e009f26
allan <allankipchumba76@gmail.com>
2023-07-23 08:33:30 +0300

    popupaled shared code between services

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export * from "./errors/badRequestError";
+export * from "./errors/customError";
+export * from "./errors/databaseConnectionError";
+export * from "./errors/notAuthorizedError";
+export * from "./errors/notFoundError";
+export * from "./errors/requestValidationError";
+
+export * from "./middleware/currentUser";
+export * from "./middleware/errorHandler";
+export * from "./middleware/requireAuth";
+export * from "./middleware/validateRequest";
